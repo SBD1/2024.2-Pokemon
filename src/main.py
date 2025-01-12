bc0d5683bf1016bbaaef4a9dceea6c4915a402ad
davi-aguiar-vieira <davideaguiarvieira@gmail.com>
2025-01-12 00:13:22 -0300

    adiciona text art e questionary

--- src/main.py
+++ src/main.py
@@ -1,15 +1,49 @@
 import psycopg2
+from art import text2art
+import questionary
 
-con = psycopg2.connect(
-    host='localhost',
-    database='pokemon',
-    user='postgres',
-    password='password',
-    port='5434'
-)
+def welcome_screen():
+    print("Bem vindo ao jogo!")
+    print(text2art("Pokemon"))
 
-cur = con.cursor()
-cur.execute('SELECT * FROM Pokemon')
-res = cur.fetchall()
-print(res)
-con.close()
+def main_menu():
+    questions = questionary.select(
+        "Escolha uma opção:",
+        choices=[
+            "Novo Jogo",
+            "Carregar Jogo",
+            "Sair"
+        ]
+    ).ask()
+    return questions
+
+def main():
+    welcome_screen()
+    option = main_menu()
+
+    if option == 'Novo Jogo':
+        print("Iniciando um novo jogo...")
+        # Adicione a lógica para iniciar um novo jogo aqui
+    elif option == 'Carregar Jogo':
+        print("Carregando jogo salvo...")
+        # Adicione a lógica para carregar um jogo salvo aqui
+    elif option == 'Sair':
+        print("Saindo do jogo...")
+        return
+
+    con = psycopg2.connect(
+        host='localhost',
+        database='pokemon',
+        user='postgres',
+        password='password',
+        port='5434'
+    )
+
+    cur = con.cursor()
+    cur.execute('SELECT * FROM Pokemon')
+    res = cur.fetchall()
+    print(res)
+    con.close()
+
+if __name__ == "__main__":
+    main()
